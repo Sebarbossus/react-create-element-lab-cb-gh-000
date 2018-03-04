a5229539ebeaf2da55af1ba3f05d48357f412573
Nico Alvarez <nicoalvareza22@gmail.com>
2018-03-04 13:48:10 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+
 ReactDOM.render(
   meInReact,
   document.getElementById('global')
 );
 
 export default meInReact
-*/
+
